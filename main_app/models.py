dd09cf162e96bea4dd1f666ff256a0e3da588e61
Jelena810-bytes <andrijevic.j@gmail.com>
2025-12-02 08:15:52 +0100

    homework

---
+++ main_app/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+# Create your models here.
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+# Add the Toy model
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0])
+
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    # def fed_for_today(self):
+    # return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+    class Meta:
+        ordering = ['-date']
+
---
+++ main_app/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Cat, Feeding, Toy
+
+
+class ToySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Toy
+    fields = '__all__'
+
+
+
+
+class CatSerializer(serializers.ModelSerializer):
+   fed_for_today = serializers.SerializerMethodField()
+   toys = ToySerializer(many=True, read_only=True) #add this line
+
+class Meta:
+    model = Cat
+    fields = '__all__'
+
+def get_fed_for_today(self, obj):
+    return obj.fed_for_today()
+
+
+
+class FeedingSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Feeding
+    fields = '__all__'
+    read_only_fields = ('cat',)
+
+
+
+
+
+
